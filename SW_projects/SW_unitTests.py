6e7d8565318f97535aea1ac03678ceb599382c33
andre <andresmichel92@hotmail.com>
2019-02-07 19:18:48 -0600

    test cases added

--- SW_projects/SW_unitTests.py
+++ SW_projects/SW_unitTests.py
@@ -2,6 +2,7 @@ import unittest
 import filecmp
 import os
 import time
+from datetime import timedelta
 
 
 class Test_fcm_method(unittest.TestCase):
@@ -11,16 +12,33 @@ class Test_fcm_method(unittest.TestCase):
         x = os.path.dirname(r'')
         self.assertFalse(filecmp.cmp("file1.txt", "file2.txt"))
 
+    # Test if two different files are equal
+    def test_two(self):
+        x = os.path.dirname(r'')
+        self.assertFalse(filecmp.cmp("file1.txt", "file3.txt"))
+
 
 class test_time_method(unittest.TestCase):
+    def setUp(self):
+        self._started_at = time.time()
 
-    # Test if time
+    def tearDown(self):
+        elapsed = time.time() - self._started_at
+        print('{} ({}s)'.format(self.id(), round(elapsed, 2)))
+
+    def test_fast(self):
+        self.assertEqual(1, 1)
+
+    def test_slow(self):
+        time.sleep(0.5)
+        self.assertEqual(1, 1)
+
+
+class test_time2_method(unittest.TestCase):
     def test_t_one(self):
-        gm_time = time.gmtime()
-        local_time = time.localtime()
-        self.assertTrue(int(gm_time)-(6*60*24) == int(time.localtime()))
-
-
+        local_time_month = time.localtime().tm_mon
+        print(local_time_month)
+        self.assertEqual(local_time_month, 2)
 
 
 if __name__ == '__main__':
