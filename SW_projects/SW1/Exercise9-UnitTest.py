132364252668b03c6aefd064ddd9f8ef2564fc44
andre <andresmichel92@hotmail.com>
2019-02-14 01:40:46 -0600

    updates

--- SW_projects/SW1/Exercise9-UnitTest.py
+++ SW_projects/SW1/Exercise9-UnitTest.py
@@ -1,5 +1,17 @@
 from SW_projects.SW1 import Exercise9 as E9
 import unittest
+from unittest import mock
 
 
-E9.ask_number()
+class TestExercise9(unittest.TestCase):
+
+    def test_ask_number(self):
+        with mock.patch('builtins.input', return_value='1'):
+            assert E9.ask_number() == int(1)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
