2960538e951dff5a4c626f8879e25519fc72b7f5
andre <andresmichel92@hotmail.com>
2019-02-14 19:45:16 -0600

    coverage test

--- SW_projects/SW1/Exercise9-UnitTest.py
+++ SW_projects/SW1/Exercise9-UnitTest.py
@@ -1,4 +1,4 @@
-from SW_projects.SW1 import Exercise9 as E9
+import Exercise9 as E9
 import unittest
 from unittest import mock
 
@@ -9,6 +9,21 @@ class TestExercise9(unittest.TestCase):
         with mock.patch('builtins.input', return_value='1'):
             assert E9.ask_number() == int(1)
 
+    def test_small_romans(self):
+        variable_t = 123
+        assert type(E9.generate_romans(variable_t))== type("string")
+
+    def test_large_romans(self):
+        variable_t = 4500
+        assert type(E9.generate_romans(variable_t))== type("string")
+
+    def test_negative_romans(self):
+        variable_t = -4500
+        assert type(E9.generate_romans(variable_t)) == type("string")
+
+    def test_million_romans(self):
+        variable_t = 1000000
+        assert E9.generate_romans(variable_t) == "_\nM"
 
 
 
