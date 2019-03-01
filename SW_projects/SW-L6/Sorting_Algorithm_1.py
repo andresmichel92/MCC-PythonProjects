464377750fd0d4aa43cf50c98d9d1941c4573932
andre <andresmichel92@hotmail.com>
2019-02-28 23:01:09 -0600

    adding merge_sort function

--- SW_projects/SW-L6/Sorting_Algorithm_1.py
+++ SW_projects/SW-L6/Sorting_Algorithm_1.py
@@ -27,13 +27,47 @@ def set_output_data(file_path_name, sorted_list):
         wr.writerow(sorted_list)
 
 
+def execute_merge_sort(unsorted_list):
+
+    if len(unsorted_list) > 1:
+        mid = int(len(unsorted_list)/2)
+        right_half = unsorted_list[:mid]
+        left_half = unsorted_list[mid:]
+
+        execute_merge_sort(right_half)
+        execute_merge_sort(left_half)
+
+        i = 0
+        j = 0
+        k = 0
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                unsorted_list[k] = left_half[i]
+                i = i + 1
+            else:
+                unsorted_list[k] = right_half[j]
+                j = j + 1
+            k = k + 1
+
+        while i < len(left_half):
+            unsorted_list[k] = left_half[i]
+            i = i + 1
+            k = k + 1
+
+        while j < len(right_half):
+            unsorted_list[k] = right_half[j]
+            j = j + 1
+            k = k + 1
+
+    return unsorted_list
+
+
 def main():
 
     aList = set_input_data("test1.csv")
     print(aList)
-    set_output_data('test2.csv', aList)
-
-
+   # set_output_data('test2.csv', aList)
+    print(execute_merge_sort(aList))
 
 
 if __name__ == '__main__':
