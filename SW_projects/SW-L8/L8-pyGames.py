b72ced7a3c07355db7c6cb786e5209c00431faf6
andre <andresmichel92@hotmail.com>
2019-03-21 20:47:33 -0600

    adding tic tac toe L8

--- SW_projects/SW-L8/L8-pyGames.py
+++ SW_projects/SW-L8/L8-pyGames.py
@@ -1,51 +1,64 @@
-import L8_UI as UI
-import os
-import tkinter as tk
+import tkinter as Tk
+turn = 0
 
-
-
-class Player:
-    def __init__(self, symbol):
-        self.symbol=symbol
-
-class Game:
-    def __init__(self, board, player1, player2):
+class Tile:
+    def __init__(self, pos_x, pos_y, board, root):
+        self.pos_x=pos_x
+        self.pos_y=pos_y
         self.board = board
-        self.player1 = player1
-        self.player2 = player2
-        self.player_turn = player1
+        self.btn= Tk.Button(root, text=self.board.board[pos_x][pos_y], fg="Black",width=3,height=1,font=('Helvetica','20'), command=self.board.on_click(pos_x, pos_y))
+        self.btn.grid(column=pos_x, row=pos_y)
 
-    def turn(self):
-        if self.player_turn == self.player1:
-            self.player_turn = self.player2
-        else:
-            self.player_turn= self.player1
-
-    def play_game(self):
-        #method to get player input
-        UI.show_game(self.board)
-        return input
-
+# model
 class Board:
-    def __init__(self, tiles, name):
-        self.tiles = tiles
+
+    def __init__(self, n_tiles, name):
+        self.n_tiles = n_tiles
         self.name = name
-        self.board = [" "] * self.tiles
+        self.board = [[" "] * self.n_tiles]*self.n_tiles
+        self.the_tiles=[]
 
-class TTTBoard(Board):
+    def get_board(self, root):
+        for i in range(len(self.board)):
+            for j in range(len(self.board[i])):
+                newTile = Tile(i,j,self,root)
+                self.the_tiles.append(newTile)
 
-    def show_board(self):
-        self.board()
+    def on_click(self, i, j):
+        global turn
+        if turn%2==0:
+            self.board[i][j]= "X"
+        elif turn>0:
+            self.board[i][j]="O"
+        turn = turn + 1
+
+# View
+class Screen:
+    def __init__(self, root, board, turn):
+        self.frame = Tk.Frame(root)
+        self.board = board
+        self.lbl = Tk.Label(root,text="turn: " + turn, font=('Helvetica', 10))
+        self.lbl.grid(row=1,column=5)
+        root.geometry("300x200")
+
+
+class Controller:
+    def __init__(self):
+        self.root = Tk.Tk()
+        self.board = Board(3, "Tic Tac Toe game")
+        self.Screen = Screen(self.root, self.board, turn=" ")
+
+    def run(self):
+        self.root.title(self.board.name)
+        self.board.get_board(self.root)
+        # self.root.deiconify()
+        self.root.mainloop()
+
 
 def main():
-    ttt_board = Board(3, "Tic Tac Toe")
-    play_game = Player
-    player1 = Player("X")
-    player2 = Player("O")
-    TTT_game = Game(ttt_board, player1, player2)
-    TTT_game.play_game()
+    c = Controller()
+    c.run()
 
 
 if __name__ == '__main__':
     main()
-
